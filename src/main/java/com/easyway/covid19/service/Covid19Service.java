8c0de2ecb1da9e2e990f864def6f6b14374a1084
Dinesh Pinnamaneni <dinesh.p@easywaytech.com>
2020-04-07 17:12:35 +0530

    API'S

---
+++ src/main/java/com/easyway/covid19/service/Covid19Service.java
@@ -0,0 +1,23 @@
+package com.easyway.covid19.service;
+
+import com.easyway.covid19.model.Records;
+
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+public interface Covid19Service {
+    Response getUserByUserName(String userName, String password);
+
+    Response addUserInfo(String userInfo);
+
+    Response logout(String userSessionToken);
+
+    Response addRecordsDetails(List<Records> recordssObjectList);
+
+    Response updateRecordsDetailsByState(String recordsString);
+
+    Response getRecordDetailsByState(String state);
+
+    Response getAllRecordDetails() throws Exception;
+
+}
